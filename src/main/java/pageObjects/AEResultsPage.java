513b649ae40a937421481d7aadd415cd034c5d5b
Bryan Breit <brian27486@gmail.com>
2020-06-21 19:27:54 -0300

    Adding new page objects

---
+++ src/main/java/pageObjects/AEResultsPage.java
@@ -0,0 +1,10 @@
+package pageObjects;
+import org.openqa.selenium.WebDriver;
+
+//Definition of the basic objects and methods on the AliExpress page
+public class AEResultsPage extends AbstractPage{
+
+	public AEResultsPage(WebDriver driver) {
+		super(driver);
+	}
+}
